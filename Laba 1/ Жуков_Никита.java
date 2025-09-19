6d72df583664ea57dd702ee627f74941ad3a3ba9
NikiZ83 <nikita.zh83@mail.ru>
2025-09-19 12:24:02 +0300

    Начало выполнения Лабы 1

---
+++ Laba 1/ Жуков_Никита.java
@@ -0,0 +1,19 @@
+public class Primes {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); // Переход на новую строку после вывода
+    }
+    
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
